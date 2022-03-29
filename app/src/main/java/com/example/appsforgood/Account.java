069e900b020c4146d42a81d98647ec1fd16c1ea7
amithchintalapati <77210776+amithchintalapati@users.noreply.github.com>
2022-03-29 08:44:37 -0400

    Preliminary Coding - Account and Problem, and Welcome Screen

---
+++ app/src/main/java/com/example/appsforgood/Account.java
@@ -0,0 +1,48 @@
+package com.example.appsforgood;
+
+public class Account {
+	private String name;
+	private String phoneNumber;
+	private String email;
+	private String password;
+	
+	public Account(String name, String phoneNumber, String email) {
+		this.name = name;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+		this.password = password;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+
+}
