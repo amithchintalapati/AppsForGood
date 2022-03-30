ecc4aaf710f56be58b653cbe23cc57baaffcfdc7
amithchintalapati <77210776+amithchintalapati@users.noreply.github.com>
2022-03-30 14:45:34 -0400

    3/30 stuff

--- app/src/main/java/com/example/appsforgood/MainActivity.java
+++ app/src/main/java/com/example/appsforgood/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.appsforgood;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,17 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
+    public void clickSignUp(View v) {
 
-        //Hello this is Amith
-        // This is a test of the thing
+        Toast toast = Toast.makeText(getApplicationContext(), "Hello, you clicked the Sign Up Button", Toast.LENGTH_LONG);
+        Intent intent = new Intent(this, SignUpActivity.class);
 
-        int x = 0;
-
-        //Bnajfklajfklajf
-
-
-        //Hello this is Bustineeeeeee
 
     }
+
 }
