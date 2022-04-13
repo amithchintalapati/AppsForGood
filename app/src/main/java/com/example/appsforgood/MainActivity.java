f11a5837025ae4698af6bac95047993d4f23eb6f
cncai <cncain@wpi.edu>
2022-04-13 10:45:04 -0400

    Charlotte (4/13)

--- app/src/main/java/com/example/appsforgood/MainActivity.java
+++ app/src/main/java/com/example/appsforgood/MainActivity.java
@@ -6,6 +6,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
@@ -17,10 +20,12 @@ import com.google.firebase.database.ValueEventListener;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MyActivity";
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         // Write a message to the database
         FirebaseDatabase database = FirebaseDatabase.getInstance();
@@ -44,7 +49,9 @@ public class MainActivity extends AppCompatActivity {
                 Log.w(TAG, "Failed to read value.", error.toException());
             }
         });
+
     }
+
 
     public void clickSignUp(View v) {
         Toast toast = Toast.makeText(getApplicationContext(), "Hello, you clicked the Sign Up Button", Toast.LENGTH_LONG);
@@ -52,7 +59,6 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, AccountActivity.class);
         //intent.putExtra("HelloStr", "Hello");
         startActivity(intent);
-
     }
 
     public void clickSignIn(View v) {
@@ -61,5 +67,6 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, SignIn.class);
         //intent.putExtra("HelloStr", "Hello");
         startActivity(intent);
+
     }
 }
--- app/src/main/java/com/example/appsforgood/SignIn.java
+++ app/src/main/java/com/example/appsforgood/SignIn.java
@@ -6,18 +6,47 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SignIn extends AppCompatActivity {
 
+    private EditText username;
+    private EditText password;
+    private Button signIn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        username = (EditText)findViewById(R.id.editEmailAdressTextStr);
+        password = (EditText)findViewById(R.id.editTextTextPassword);
+        signIn = (Button)findViewById(R.id.signInButtonStr);
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                validate(username.getText().toString(), password.getText().toString());
+
+            }
+        });
     }
+
+    private void validate (String userName, String userPassword){
+        if((userName.equals("username")) && (userPassword.equals("1234"))){
+            Intent intent = new Intent(SignIn.this, MainScreen.class);
+            startActivity(intent);
+        }
+        else{
+            signIn.setEnabled(false);
+        }
+    }
+
     public void clickActualSignIn(View v) {
+
         Toast toast = Toast.makeText(getApplicationContext(), "Hello, you clicked the Sign In Numero Dos Button", Toast.LENGTH_LONG);
         toast.show();
         Intent intent = new Intent(this, MainScreen.class);
